ff6584b1bd81ff8111e70bc9bf52f1a923443840
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-10-20 14:40:30 -0300

    adicionando form control e responsividade ao formulario

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.html',
   styleUrl: './forms.scss'
 })
 export class Forms implements OnInit {
 
-results = [];
+  results = [];
+  formGroup! : FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
   }
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
 
+  onSubmit(): void {
+    console.log(this.formGroup.value)
+  }
+  onClean(): void {
+    this.formGroup.reset()
   }
 
 }
+
