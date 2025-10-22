2e65cd472e7c3db4ff976d7de7d27eed243aa102
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-10-22 16:47:55 -0300

    lógica das horas pt1

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -3,6 +3,24 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { GetUnits } from '../../services/get-units';
 import { Location } from '../../types/location.interface';
+import { first, last } from 'rxjs';
+
+const OPENING_HOURS = {
+  morning: {
+    first: '06',
+    last: "12"
+  },
+  afternoon: {
+    first:'12',
+    last:'18'
+  },
+  nigth: {
+    first:'18',
+    last:"23"
+  }
+}
+
+type HOUR_INDEXES = 'morning' | 'afternoon' | 'nigth'
 
 @Component({
   selector: 'app-forms',
@@ -28,7 +46,43 @@ export class Forms implements OnInit {
     });
   }
 
+  transformWeekday(weekday: number) {
+    switch(weekday) {
+      case 0:
+        return 'Dom.'
+      case 6:
+        return 'Sáb.'
+      default:
+        return 'Seg. à Sex.'
+    }
+  }
+
+  filterUnits(unit:Location,opened:boolean, open_hour: string, close_hour:string){
+    let open_hour_filter = parseInt(open_hour, 10)
+    let close_hour_filter = parseInt(close_hour, 10)
+
+    let todays_weekday  = this.transformWeekday(new Date().getDate());
+
+    for(let i = 0; i < unit.schedules.length; i++) {
+      let schedule_hour = unit.schedules[i].hour
+      let schedule_weekday = unit.schedules[i].weekdays
+      if(todays_weekday === schedule_weekday) {
+        if(schedule_hour !== 'fechada'){
+          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ')
+          let unit_open_hour_int = parseInt(unit_open_hour.replace('h', ''), 10)
+          let unit_close_hour_filter = parseInt(unit_open_hour.replace('h', ''), 10)
+        }
+      }
+
+    }
+
+    return false;
+  }
+
   onSubmit(): void {
+    const OPEN_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].first
+    const CLOSE_HOUR = OPENING_HOURS[this.formGroup.value.hour as HOUR_INDEXES].last
+
     if(!this.formGroup.value.showClosed) {
       this.filterResults = this.results.filter(location => location.opened === true)
     } else {
